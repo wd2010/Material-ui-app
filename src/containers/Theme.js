7def722f5602878f2091f746238ea94de413534e
wendan <dan.wen@360gst.com>
2018-01-25 18:35:39 +0800

    yy

--- src/containers/Theme.js
+++ src/containers/Theme.js
@@ -5,9 +5,16 @@ import styled from 'styled-components';
 const ThemeContainer=styled.div`
   padding: 10px;
 `
-const Theme=()=>
-  <ThemeContainer>
-    <Chip label="Basic Chip"  />
-  </ThemeContainer>
+
+const Theme=()=>{
+  let themeObj=['red','pink','purple','deepPurple','indigo','blue','lightBlue','cyan','teal','green',
+    'lightGreen','lime','yellow','amber','orange','deepOrange','brown','grey','blueGrey']
+  return (
+    <ThemeContainer>
+      <Chip  style={{background: 'teal',width:'80px'}} />
+    </ThemeContainer>
+  )
+}
+
 
 export default Theme
